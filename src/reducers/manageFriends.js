c8eb0c877238cb86e4e65664010d96fa3830eecc
EducationPurposesOnly <educationpurposesonly@Nayas-MacBook-Pro.local>
2020-06-02 23:41:45 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,12 @@
-export function manageFriends(state, action){
+export function manageFriends(state={friends: []}, action) {
+     switch (action.type) {
+          case 'ADD_FRIEND':
+               state.friends.push(action.friend)
+               return { friends: state.friends}
+          case 'REMOVE_FRIEND':
+               const unremovedFriends = state.friends.filter(friend => friend.id !== action.id)
+               return { friends: unremovedFriends }
+          default:
+               return state
+     }
 }
