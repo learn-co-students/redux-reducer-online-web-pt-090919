5f6258e68107787ea32fa6f28e8bd38523916e9b
Emil Andreasyan <emil.andreasyan@yahoo.com>
2020-06-04 04:27:15 +0000

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,13 +1,31 @@
-export function manageFriends(state, action){
-  state = {friends: []}
+export function manageFriends(state = {
+  friends: [],
+}, action) {
   switch(action.type) {
+
     case "ADD_FRIEND":
-      return {friends: state.friends.push({name: "Chrome Boi"
-      homewtown: "NYC",
-      id: 1}})
+      return (
+        {...state,
+            friends: [
+              ...state.friends,
+              action.friend
+            ]
+        }
+    )
+
     case "REMOVE_FRIEND":
-      return {friends: state.friends.pop()}
-      default:
-      return state
+      const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+      return (
+        {...state,
+            friends: [
+              ...state.friends.slice(0, removalIndex),
+              ...state.friends.slice(removalIndex + 1)
+            ]
+        }
+      )
+
+    default:
+      return state;
+
   }
-}
+};
